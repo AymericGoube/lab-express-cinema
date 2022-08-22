a8a32244b84a0fc54633d83338d5d19e7a316c23
Aymeric Goube <aymericgoube@hotmail.fr>
2022-08-22 09:14:33 +0200

    lab cinosh

---
+++ models/Movie.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+const movieSchema = new Schema({
+  title: {
+    type: Schema.Types.String,
+  },
+  director: {
+    type: Schema.Types.String,
+  },
+  stars: {
+    type: [Schema.Types.String],
+  },
+  image: {
+    type: Schema.Types.String,
+  },
+  description: {
+    type: Schema.Types.String,
+  },
+  showtimes: {
+    type: [Schema.Types.String],
+  },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET /
 
 This is a health check. It allows us to see that the API is running.
 */
-router.get('/', (req, res, next) =>
-  res.json({ success: true, name: 'lab-express-cinema' })
-)
+router.get("/", (req, res, next) => res.json({ success: true, name: "movie" }));
 
-module.exports = router
+// GET /movies
+router.get("/movies", async (req, res) => {
+  const everyMovies = await Movie.find().select({ title: 1, director: 1 });
+  res.json({ everyMovies });
+});
+
+router.get("/movie/:id", async (req, res) => {
+  const notEveryMovies = await Movie.findById(req.params.id);
+  res.json({ notEveryMovies });
+});
+module.exports = router;
